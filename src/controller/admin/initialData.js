e0e4b79ecdad3c7dd3e133667a58d1fe93803970
raj <paulsouraj99@gmail.com>
2021-06-14 12:52:28 +0530

    aws-sdk_integrated

--- src/controller/admin/initialData.js
+++ src/controller/admin/initialData.js
@@ -4,7 +4,10 @@ const Product = require('../../models/product')
 exports.initialData = async (req , res) => {
 
     const categories = await Category.find({}).exec();
-    const products = await Product.find({}).exec();
+    const products = await Product.find({})
+                                    .select('_id name price quantity slug description productPictures category')
+                                    // .populate('category')
+                                    .exec();
     
     res.status(200).json({
         categories,
